473dd4f7344a79abf3730146badb20e1cf9fd8b6
Danilo Augusto <daniloaugusto9101@gmail.com>
2023-05-11 16:47:38 -0300

    Bkp arquivos

--- 1 SEM/PAW - Padroes Web/exercicio 1-8-1/assets/js/main.js
+++ 1 SEM/PAW - Padroes Web/exercicio 1-8-1/assets/js/main.js
@@ -3,3 +3,17 @@
 //   .then(data => {
 //     document.getElementById("header-container").innerHTML = data;
 //   });
+
+
+const inputsNoticias = () => {
+    const obj = {
+        titulo: document.querySelector('#formNoticias .input-titulo').value,
+        conteudo: document.querySelector('#formNoticias .input-texto').value,
+        tag: document.querySelector('#formNoticias .input-tag').value,
+    };    
+    return obj;
+}
+
+const saveBanco = () => {
+    localStorage.setItem('valor chave', "valroes");
+}
